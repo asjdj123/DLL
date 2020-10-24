cdf78dce5c9354b749fda6959a5e9b7eb42d6c46
asjdj123 <73373779+asjdj123@users.noreply.github.com>
2020-10-24 11:15:06 +0800

    Add files via upload

---
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Class1
+{
+    public int add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int minus(int a, int b)
+    {
+        return a - b;
+}
+
+    public int ass(int a, int b)
+{
+    return a / b;
+}
+
+    public int umnojit(int a, int b)
+{
+    return a * b;
+}
+}
